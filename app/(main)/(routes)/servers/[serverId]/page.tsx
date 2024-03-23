69885ef72f665d807d6fbd1e1d3978c900f97bc8
shuverida <bashtarov@outlook.com>
2024-03-23 15:37:13 +0300

    Added navigation sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+
+interface IPageProps {
+}
+
+const Page: NextPage<IPageProps> = () => {
+  return (
+    <div>
+      Page 
+    </div>
+  );
+};
+export default Page
