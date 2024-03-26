b9d1d820a1d94de4f06eb96c3e6961c2b9f4edf5
shuverida <bashtarov@outlook.com>
2024-03-26 15:40:56 +0300

    Added the ability to create channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import {CreateServerModal} from "@/components/modals/create-server-modal";
 import {InviteModal} from "@/components/modals/invite-modal";
 import {EditServerModal} from "@/components/modals/edit-server-modal";
 import {MembersModal} from "@/components/modals/members-modal";
+import {CreateChannelModal} from "@/components/modals/create-channel-modal";
 
 interface IModalProviderProps {
 }
@@ -26,6 +27,7 @@ export const ModalProvider: FC<IModalProviderProps> = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };
