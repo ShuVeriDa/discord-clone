b059a0562bcb320c0e3e0840598c9c6a5ac9e5b1
shuverida <bashtarov@outlook.com>
2024-03-27 16:18:46 +0300

    added the ability to search server

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,40 +2,55 @@ import {FC} from 'react';
 import {currentProfile} from "@/lib/current-profile";
 import {redirect} from "next/navigation";
 import {db} from "@/lib/db";
-import {ChannelType} from "@prisma/client";
+import {ChannelType, MemberRole} from "@prisma/client";
 import {ServerHeader} from "@/components/server/server-header";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {ServerSearch} from "@/components/server/server-search";
+import {Hash, Mic, ShieldAlert, ShieldCheck, Video} from "lucide-react";
 
 interface IServerSidebarProps {
   serverId: string
 }
 
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />
+};
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />
+}
+
 export const ServerSidebar: FC<IServerSidebarProps> = async ({serverId}) => {
-  const profile = await currentProfile()
+  const profile = await currentProfile();
 
   if (!profile) {
-    return redirect("/")
+    return redirect("/");
   }
 
   const server = await db.server.findUnique({
     where: {
-      id: serverId
+      id: serverId,
     },
     include: {
       channels: {
         orderBy: {
-          createdAt: "asc"
-        }
+          createdAt: "asc",
+        },
       },
       members: {
         include: {
-          profile: true
+          profile: true,
         },
         orderBy: {
-          role: "asc"
+          role: "asc",
         }
       }
     }
-  })
+  });
 
   const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
   const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
@@ -43,14 +58,61 @@ export const ServerSidebar: FC<IServerSidebarProps> = async ({serverId}) => {
   const members = server?.members.filter((member) => member.profileId !== profile.id)
 
   if (!server) {
-    return redirect("/")
+    return redirect("/");
   }
 
-  const role = server.members.find((member) => member.profileId === profile.id)?.role
+  const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]">
-      <ServerHeader server={server} role={role}/>
+      <ServerHeader
+        server={server}
+        role={role}
+      />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                }))
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                }))
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                }))
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members?.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                }))
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
