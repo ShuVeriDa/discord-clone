c7c7b1ab805d94c73074bf6aac1c97db913a7d92
shuverida <bashtarov@outlook.com>
2024-04-08 12:16:34 +0300

    socket io setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import {FC} from 'react';
 import {Hash, Menu} from "lucide-react";
 import {MobileToggle} from "@/components/mobile-toggle";
 import {UserAvatar} from "@/components/user-avatar";
+import {SocketIndicator} from "@/components/socket-indicator";
 
 interface IChatHeaderProps {
   serverId: string
@@ -28,6 +29,9 @@ export const ChatHeader: FC<IChatHeaderProps> = (
       <p className="font-semibold text-md text-black dark:text-white">
         {name}
       </p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator/>
+      </div>
     </div>
   );
 };
